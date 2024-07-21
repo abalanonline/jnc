1b8b2618c3640cf37960e2912919929446b93feb
Aleksei Balan <28422872+abalanonline@users.noreply.github.com>
2024-07-21 14:21:33 -0400

    2024-07-21 18:21:33+00:00

--- src/main/java/ab/jnc3/Spectrogram.java
+++ src/main/java/ab/jnc3/Spectrogram.java
@@ -11,6 +11,7 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 
 public class Spectrogram {
 
@@ -21,18 +22,16 @@ public class Spectrogram {
   public static final int FFT_HEIGHT = 160;
   public static final double[] LOG = new double[FFT_HEIGHT];
 
+  int mode;
+
   static {
     for (int i = 0, d = -FFT_HEIGHT; i < FFT_HEIGHT; i++) {
       LOG[i] = Math.exp(++d / 30.0) * (FFT_LENGTH / 2.0 - 0.01);
     }
   }
 
-  public static double[] logarithmic(Complex[] source) {
-    int n = source.length / 2;
-    int n1 = source.length - 1;
-    double[] f = new double[n];
-    for (int i = 0; i < n; i++) f[i] = source[i].abs() + source[n1 - i].abs();
-    n = LOG.length;
+  public static double[] logarithmic(double[] f) {
+    int n = LOG.length;
     double[] result = new double[n];
     double pivot = 0;
     for (int i = 0; i < n; i++) {
@@ -47,9 +46,36 @@ public class Spectrogram {
     return result;
   }
 
+  public static void drawDoubles(BufferedImage image, double[] doubles, double mul, int di,
+      int x, int xi, int y, int yi, int color) {
+    for (int i0 = doubles.length / di, i = 0; i0 > 0; i0--) {
+      double sum = 0;
+      for (int i1 = 0; i1 < di; i1++) sum += Math.abs(doubles[i++]);
+      sum = Math.min(sum * mul / di, 1);
+      int c = (int) ((color & 0xFF0000) * sum) & 0xFF0000
+          | (int) ((color & 0x00FF00) * sum) & 0x00FF00
+          | (int) ((color & 0x0000FF) * sum) & 0x0000FF;
+      image.setRGB(x, y, c);
+      x += xi;
+      y += yi;
+    }
+
+  }
+
+  public static double[] fftTransform(double[] audio) {
+    int n = audio.length;
+    int n2 = n / 2;
+    Complex[] fftComplex = FFT.transform(audio, TransformType.FORWARD);
+    double[] fft = new double[n];
+    for (int i = 0; i < n2; i++) fft[i] = fftComplex[i].abs();
+    return fft;
+  }
+
   public static void main(String[] args) throws LineUnavailableException {
     Screen screen = new Screen();
+    Spectrogram sp = new Spectrogram();
     screen.keyListener = key -> {
+      if (key.equals("Tab")) sp.mode = (sp.mode + 1) % 6;
       if (key.equals("Esc")) System.exit(0);
     };
     BufferedImage image = screen.image;
@@ -59,32 +85,39 @@ public class Spectrogram {
     int w1 = w - 1;
 
     AudioFormat format = AUDIO_CD_MONO;
-    int length = FFT_LENGTH * format.getFrameSize();
-    byte[] bytes = new byte[length];
-    double[] doubles = new double[FFT_LENGTH];
+    final int frameSize = format.getFrameSize();
+    int fftSize = FFT_LENGTH;
+    byte[] bytes = new byte[0x10000];
 
     TargetDataLine line = AudioSystem.getTargetDataLine(format);
-    line.open(format, (int) (format.getFrameRate() * BUFFER_MILLISECONDS / 1000) * format.getFrameSize());
+    line.open(format, (int) (format.getFrameRate() * BUFFER_MILLISECONDS / 1000) * frameSize);
     line.start();
 
-    while (line.read(bytes, 0, length) == length) {
-      for (int i = 0, j = 0; i < FFT_LENGTH; i++) {
-        final byte l = bytes[j++];
-        doubles[i] = (bytes[j++] << 8 | l) / 32768.0;
+    while (line.read(bytes, 0, fftSize * frameSize) == fftSize * frameSize) {
+      double[] audio = new double[fftSize];
+      for (int i = 0, j = 0; i < fftSize; i++) {
+        final int l = bytes[j++] & 0xFF;
+        audio[i] = (bytes[j++] << 8 | l) / 32768.0;
       }
-      Complex[] fft = FFT.transform(doubles, TransformType.FORWARD);
-      double[] f = logarithmic(fft);
+      double[] fft = fftTransform(audio);
+      if (sp.mode == 0) fft = logarithmic(fft);
       graphics.drawImage(image, -1, 0, null);
-      for (int i = 0, y = FFT_HEIGHT - 1, x = 0; i < FFT_HEIGHT; i++, y--, x++) {
-        int v = (int) Math.min(Math.round(f[x] * 5), 127);
-        image.setRGB(w1, y, v * 0x020101);
-      }
-      for (int i = h - FFT_HEIGHT - 1, y = FFT_HEIGHT, j = 1; i >= 0; i--, y++, j += 2) {
-        int v = bytes[j];
-        if (v < 0) v = - v - 1;
-        image.setRGB(w1, y, v * 0x020101);
+      if (sp.mode == 0) {
+        drawDoubles(image, fft, 0.05, 1, w1, 0, FFT_HEIGHT - 1, -1, 0xFF8080);
+        drawDoubles(image, Arrays.copyOf(audio, h - FFT_HEIGHT), 1.0, 1, w1, 0, FFT_HEIGHT, 1, 0xFF8080);
+      } else {
+        drawDoubles(image, Arrays.copyOf(fft, fft.length / 2), 0.05, fftSize / 256, w1, 0, 127, -1, 0xFFFF80);
+        drawDoubles(image, Arrays.copyOf(audio, h - 128), 1.0, 1, w1, 0, 128, 1, 0xFFFF80);
       }
       screen.update();
+      switch (sp.mode) {
+        case 0: fftSize = FFT_LENGTH; break;
+        case 1: fftSize = 512; break;
+        case 2: fftSize = 1024; break;
+        case 3: fftSize = 2048; break;
+        case 4: fftSize = 4096; break;
+        case 5: fftSize = 8192; break;
+      }
     }
   }
 }
